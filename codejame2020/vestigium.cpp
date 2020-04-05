6c04e63ffc6cc717ef0c6398136535790b9cac28
Sirn <sonnt174@gmail.com>
2020-04-05 10:24:23 +0100

    Update vestigium.cpp

--- codejame2020/vestigium.cpp
+++ codejame2020/vestigium.cpp
@@ -1,3 +1,7 @@
+// Copyright (c) 2020 Sirn Nguyen Truong <sonnt174@gmail.com>
+//
+// Problem: https://codingcompetitions.withgoogle.com/codejam/round/000000000019fd27/000000000020993c
+
 #include <iostream>
 #include <vector>
 
